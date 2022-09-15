815c896270f8e9b662e57c6cdc8d99c67eba01c3
hamidreza <takloohamidreza@gmail.com>
2022-09-15 16:59:47 +0430

    Add missed new keyword before using object

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ document.addEventListener('click',(e)=>{
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pageNumber = document.getElementById("pageNumber").value;
-    const book = Book(author,title,pageNumber);
+    const book =  new Book(author,title,pageNumber);
     addToLibrary(book);
     togglePopup();
 })
